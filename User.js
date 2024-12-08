f8289f4feaa8b01f5372ef386e610b4bf83531d8
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-12-08 20:39:43 +0530

    first commit

---
+++ User.js
@@ -0,0 +1,24 @@
+const users=[
+    {
+        id:1,
+        name:"john",
+        email:"john@gmail.com"
+    },
+    {
+        id:2,
+        name:"smith",
+        email:"smith@gmail.com"
+    },
+    {
+        id:3,
+        name:"jack",
+        email:"jack@gmail.com"
+    },
+    {
+        id:4,
+        name:"chris",
+        email:"chris@gmail.com"
+    }
+]
+
+module.exports=users;
